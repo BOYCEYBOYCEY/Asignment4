474d7e4d2a547d4cabb31ccab305fa4cb6be048c
BOYCEYBOYCEY <88803873+BOYCEYBOYCEY@users.noreply.github.com>
2024-02-25 13:16:37 +0530

    Add files via upload

---
+++ Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using Asign4Vishal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Asign4Vishal.Controllers
+{
+    public class UserController : Controller
+    {
+        MyContext db=new MyContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            return RedirectToAction("ShowUser");
+        }
+
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+        public ActionResult CreateUser()
+        {
+            return RedirectToAction("AddUser");
+        }
+
+
+
+        [HttpPost]
+        public ActionResult AddUser(User u)
+        {
+            db.users.Add(u);
+            db.SaveChanges();
+
+            return View();
+        }
+
+        public ActionResult ShowUser()
+        {
+            return View(db.users.ToList());
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var item=db.users.Find(id);
+
+            if(item != null)
+            {
+                db.users.Remove(item);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ShowUser");   
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var item = db.users.Find(id);
+
+            return View(item);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User u)
+        {
+            db.Entry(u).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("ShowUser");
+
+
+           
+        }
+
+
+
+
+    }
+}
---
+++ Models/MyContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Asign4Vishal.Models
+{
+    public class MyContext:DbContext
+    {
+       public MyContext() : base("MyContext") {
+        
+        }
+
+       public  DbSet<User> users {  get; set; }
+
+
+    }
+
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Designation { get; set;}
+    
+    
+    
+    }
+
+
+}
